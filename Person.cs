5694cde3671a29ae4122da3fa7c4c56df44c611d
Muhannad Aljouri <muhannad.aljouripay@gmail.com>
2025-12-25 17:02:19 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labbb4prgram
+{
+    // Enum för kön
+    enum Gender
+    {
+        Kvinna,
+        Man,
+        IckeBinar,
+        Annan
+    }
+
+    // Struct för hår
+    struct Hair
+    {
+        public string Langd;
+        public string Farg;
+    }
+
+    // Klass för person
+    class Person
+    {
+        public string Namn;
+        public Gender Kon;
+        public Hair Hår;
+        public DateTime Fodelsedag;
+        public string Ögonfarg;
+
+        public string ToText()
+        {
+            return
+                "Namn: " + Namn + "\n" +
+                "Kön: " + Kon + "\n" +
+                "Hår: " + Hår.Langd + ", " + Hår.Farg + "\n" +
+                "Födelsedag: " + Fodelsedag.ToShortDateString() + "\n" +
+                "Ögonfärg: " + Ögonfarg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace labbb4prgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa hår
+            Hair har = new Hair();
+            har.Langd = "Kort";
+            har.Farg = "gul";
+
+            // Skapa person
+            Person person = new Person();
+            person.Namn = "Bakir";
+            person.Kon = Gender.Man;
+            person.Hår = har;
+            person.Fodelsedag = new DateTime(1590, 4, 15);
+            person.Ögonfarg = "Blå";
+
+            // Skriv ut
+            Console.WriteLine(person.ToText());
+        }
+    }
+}
