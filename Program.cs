efa522af1b2dea81da2c38a0325ef55f5b8c29bf
muhannad <muhannad.aljouripay@gmail.com>
2026-01-04 20:25:42 +0100

    fungerade sprint 2

--- Program.cs
+++ Program.cs
@@ -5,39 +5,48 @@ namespace labbb4prgram
 {
     class Program
     {
-        // Lista som sparar alla personer
+        // en lista som sparar alla personer som läggs till den
         static List<Person> personer = new List<Person>();
 
         static void Main(string[] args)
         {
+            // detta är en bool som styr om programmet ska fortsätta köra eller inte
             bool run = true;
 
+            
             while (run)
             {
-                Console.Clear();
+                
+               
                 Console.WriteLine("MENY");
                 Console.WriteLine("1. Lägg till person");
                 Console.WriteLine("2. Visa alla personer");
                 Console.WriteLine("3. Avsluta");
                 Console.Write("Välj: ");
 
+                // Läser in användarens val alltså lässer om användaren vill lägga till person, visa personer eller avsluta
                 string val = Console.ReadLine();
 
+                
                 switch (val)
                 {
                     case "1":
+                        
                         AddPerson();
                         break;
 
                     case "2":
+                        
                         ListPersons();
                         break;
 
                     case "3":
+                        
                         run = false;
                         break;
 
                     default:
+                        // Om användaren skriver något annat än en bokstav eller en siffra som inte finns i menyn så får den detta meddelande om att det är fel val
                         Console.WriteLine("Fel val. Tryck Enter.");
                         Console.ReadLine();
                         break;
@@ -45,18 +54,20 @@ namespace labbb4prgram
             }
         }
 
-        // =====================
-        // LÄGG TILL PERSON
-        // =====================
+       
+        //  FÖR ATT LÄGGA TILL EN PERSON
+       
         static void AddPerson()
         {
-            Console.Clear();
+
+            // Skapar ett nytt person objekt
             Person p = new Person();
 
+            
             Console.Write("Namn: ");
             p.Namn = Console.ReadLine();
 
-            // Kön (enkelt)
+            
             while (true)
             {
                 Console.Write("Kön (0=Kvinna, 1=Man, 2=IckeBinar, 3=Annan): ");
@@ -64,6 +75,7 @@ namespace labbb4prgram
 
                 if (kon >= 0 && kon <= 3)
                 {
+                    
                     p.Kon = (Gender)kon;
                     break;
                 }
@@ -73,18 +85,21 @@ namespace labbb4prgram
                 }
             }
 
-            // Hår
+            // Skapar ett Hair objekt
             Hair har = new Hair();
 
+            // Läser in hårlängd
             Console.Write("Hårlängd: ");
             har.Langd = Console.ReadLine();
 
+            // Läser in hårfärg
             Console.Write("Hårfärg: ");
             har.Farg = Console.ReadLine();
 
+            // Sparar hår informationen i personen
             p.Har = har;
 
-            // Födelsedatum (enkelt + säkert)
+            // Läser in födelsedatum (med felhantering om användaren väler fel år eller månad så för de en felmeddelande )
             while (true)
             {
                 try
@@ -98,41 +113,46 @@ namespace labbb4prgram
                     Console.Write("Födelsedag: ");
                     int dag = int.Parse(Console.ReadLine());
 
+                    // Skapar ett DateTime objekt
                     p.Fodelsedag = new DateTime(år, månad, dag);
                     break;
                 }
                 catch
                 {
+                    
                     Console.WriteLine("Fel datum. Försök igen.");
                 }
             }
 
+            // Läser in ögonfärg
             Console.Write("Ögonfärg: ");
             p.Ogonfarg = Console.ReadLine();
 
-            // Lägg till i listan
+            // Lägger till personen i listan
             personer.Add(p);
 
-            Console.WriteLine("Personen är tillagd!");
-            Console.WriteLine("Tryck Enter för att fortsätta.");
+            Console.WriteLine("Personen är tillagd");
+            Console.WriteLine("Tryck Enter för att fortsätta."); // Meddelande för att informera användaren att personen har lagts till och att trycka Enter för att fortsätta med den andra person eller avsluta.
             Console.ReadLine();
         }
 
-        
-        // VISA ALLA PERSONER
-        
+       
+        //  FÖR ATT VISA ALLA PERSONER
+      
         static void ListPersons()
         {
-            Console.Clear();
 
+          
             if (personer.Count == 0)
             {
                 Console.WriteLine("Inga personer finns.");
             }
             else
             {
+                // Loopar igenom alla personer i listan
                 foreach (Person p in personer)
                 {
+                    // Skriver ut personens information
                     Console.WriteLine(p.ToString());
                     Console.WriteLine("--------------------");
                 }
